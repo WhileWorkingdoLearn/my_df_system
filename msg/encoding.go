2b0b579eef43a6538b710f4ba7e6a7270482c5e7
s_ms_on_github <sommer_ste@gmx.de>
2025-03-27 15:43:50 +0100

    Added Encoder Structure

--- msg/encoding.go
+++ msg/encoding.go
@@ -1,1 +1,16 @@
 package msg
+
+type Encoder struct {
+}
+
+func (e *Encoder) EncodeString(data string) ([]byte, error) {
+	return nil, nil
+}
+
+func (e *Encoder) parseString(data string) (int, bool, error) {
+	return 0, false, nil
+}
+
+func (e *Encoder) Encode(data Message) ([]byte, error) {
+	return nil, nil
+}
